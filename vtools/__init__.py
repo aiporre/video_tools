68916f68b29ead4aaccf22d7068c39f256f199c3
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-10-18 16:51:17 +0200

    generic videotoimage conversion no transformations

--- vtools/__init__.py
+++ vtools/__init__.py
@@ -1,5 +1,5 @@
 from .denoising import ContrastDenoiser, N2VDenoiser
 from .trainN2V import prepare_training_data, generate_args, train_model
 from .videotogray import VideoToGray
-from .videotoimage import VideoToGrayImage
+from .videotoimage import VideoToGrayImage, VideoToImage
 from .videotovideo import VideoToVideo
--- vtools/videotoimage.py
+++ vtools/videotoimage.py
@@ -1,37 +1,36 @@
 import cv2
 import argparse
-import os 
+import os
+from tqdm import tqdm
 
-class VideoToGrayImage(object):
+
+class VideoToImage(object):
     def __init__(self, src=0, output_path = './', extension = '.jpg'):
         # Create a VideoCapture object
         self.capture = cv2.VideoCapture(src)
         self.output_path = output_path
         self.frame_counter = 0
-        # Default resolutions of the frame are obtained (system dependent)
+        # resolution of the video
         self.frame_width = int(self.capture.get(3))
         self.frame_height = int(self.capture.get(4))
+        self.n_frames = int(self.capture.get(7))
         self.extension = extension
 
-
     def update(self):
-        # Read the next frame 
+        # Read the next frame
         if self.capture.isOpened():
             (self.status, self.frame) = self.capture.read()
             self.frame_counter +=1
-        if self.status:
-            self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
     def show_frame(self):
         # Convert to grayscale and display frames
         if self.status:
-            cv2.imshow('grayscale frame', self.gray)
+            cv2.imshow('frame', self.frame)
 
         # Press 'q' on keyboard to stop recording
         key = cv2.waitKey(1)
         if key == ord('q'):
             self.capture.release()
-            self.output_video.release()
             cv2.destroyAllWindows()
             exit(1)
 
@@ -39,13 +38,47 @@ class VideoToGrayImage(object):
         # Save grayscale frame into video output file
         if self.status: # self.capture.isOpened():
             filename = os.path.join(self.output_path,"frame_"+str(self.frame_counter) + self.extension)
-            cv2.imwrite(filename, self.gray)
-            if os.path.exists(filename):
-                print('file :', filename, 'created')
-            else:
-                print('file :', filename, 'failed')
+            cv2.imwrite(filename, self.frame)
+
+    def close(self):
+        self.capture.release()
+        cv2.destroyAllWindows()
+        exit(1)
+
+
+class VideoToGrayImage(VideoToImage):
+    def __init__(self, src=0, output_path = './', extension = '.jpg'):
+        super(VideoToImage,self).__init__(src=src, output_path = output_path, extension = extension)
+
+    def update(self):
+        super().update()
+        if self.status:
+            self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
+
+def run(video_src, video_out=None, extension = '.png',plot='y'):
+    '''
+    run default video to image
+    '''
+    if video_out is None:
+        video_out = os.path.dirname(video_src)
+        video_out = os.path.join(video_out,'converted.avi')
+
+    video_stream_widget = VideoToGrayImage(video_src, output_path = video_out, extension = extension)
+    if plot == 'y':
+        print('stop convertion by pressing q')
+    for _ in tqdm(range(video_stream_widget.n_frames)):
+        if video_stream_widget.capture.isOpened():
+            try:
+                video_stream_widget.update()
+                if plot == 'y':
+                    video_stream_widget.show_frame()
+                video_stream_widget.save_frame()
+            except AttributeError:
+                pass
         else:
-            print('Capure stream is closed')
+            video_stream_widget.close()
+    video_stream_widget.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert to gray avi videos.')
