fix denoising pre normalization

vtools/denoising.py
@@ -8,7 +8,15 @@ from matplotlib.image import imread, imsave
 from csbdeep.io import save_tiff_imagej_compatible
 from skimage import exposure
 
-
+def normalize(img):
+    if len(img.shape) == 2:
+        img = (img-img.min())/(img.max()-img.min())
+    elif len(img.shape) == 3:
+        for i in range(3):
+            img[...,i] = (img[...,i]-img[...,i].min())/(img[...,i].max()-img[...,i].min())
+    else:
+        raise Exception('image must be YX or YXC. Shape found Found '+ str(img.shape))
+    return img
 
 class N2VDenoiser(object):
     def __init__(self, axes='YX'):
@@ -21,6 +29,8 @@ class N2VDenoiser(object):
         self.axes = axes
 
     def __call__(self,img):
+        if img.max()>1.0 or img.min()<0.0:
+            img = normalize(img)
         pred = self.model.predict(img, axes=self.axes)
         pred = np.clip(pred,0.0,1.0)
         return pred