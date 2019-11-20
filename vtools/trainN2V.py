a4fb124e87da35ca0abc21bd965eb95277910068
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-11-20 14:57:18 +0100

    generate args

--- vtools/trainN2V.py
+++ vtools/trainN2V.py
@@ -7,29 +7,32 @@ from n2v.internals.N2V_DataGenerator import N2V_DataGenerator
 import numpy as np
 print('everything imported')
 
-def generate_args(data_path, fileName = '*.png', dims = 'XY'):
+def generate_args(data_path, fileName = '*.png', dims = 'XY', baseDir = 'models', name = 'N2V',
+                  validationFraction = 5.0, patchSizeXY=64, patchSizeZ=64, epochs=100, stepsPerEpoch = 400,
+                  batchSize = 64, netDepth=2, netKernelSize=3, n2vPercPix=1.6, learningRate = 0.0004,
+                  unet_n_first = 32):
     '''
     Generates the arguments to prepare data
     :param data_path: path to the training_images
     :return: dictionary of arguments
     '''
     args = {}
-    args["baseDir"] = 'models'  # help="base directory in which your network will live"
-    args["name"] = 'N2V'  # help="name of your network"
+    args["baseDir"] = baseDir  # help="base directory in which your network will live"
+    args["name"] = name  # help="name of your network"
     args["dataPath"] = data_path  # help="The path to your training data"
     args["fileName"] = fileName  # help="name of your training data file"
-    args["validationFraction"] = 5.0  # help="Fraction of data you want to use for validation (percent)"
+    args["validationFraction"] = validationFraction # help="Fraction of data you want to use for validation (percent)"
     args["dims"] = dims  # help="dimensions of your data, can include: X,Y,Z,C (channel), T (time)"
-    args["patchSizeXY"] = 64  # help="XY-size of your training patches"
-    args["patchSizeZ"] = 64  # , help="Z-size of your training patches"
-    args["epochs"] = 100  #  help="number of training epochs"
-    args["stepsPerEpoch"] = 400  # help="number training steps per epoch"
-    args["batchSize"] = 64  # help="size of your training batches"
-    args["netDepth"] = 2  # help="depth of your U-Net"
-    args["netKernelSize"] = 3  # help="Size of conv. kernels in first layer"
-    args["n2vPercPix"] = 1.6  # help="percentage of pixels to manipulated by N2V"
-    args["learningRate"] = 0.0004  # help="initial learning rate"
-    args["unet_n_first"] = 32  # help="number of feature channels in the first u-net layer"
+    args["patchSizeXY"] = patchSizeXY  # help="XY-size of your training patches"
+    args["patchSizeZ"] = patchSizeZ  # , help="Z-size of your training patches"
+    args["epochs"] = epochs  #  help="number of training epochs"
+    args["stepsPerEpoch"] = stepsPerEpoch  # help="number training steps per epoch"
+    args["batchSize"] = batchSize  # help="size of your training batches"
+    args["netDepth"] = netDepth  # help="depth of your U-Net"
+    args["netKernelSize"] = netKernelSize  # help="Size of conv. kernels in first layer"
+    args["n2vPercPix"] = n2vPercPix  # help="percentage of pixels to manipulated by N2V"
+    args["learningRate"] = learningRate  # help="initial learning rate"
+    args["unet_n_first"] = unet_n_first  # help="number of feature channels in the first u-net layer"
 
     return argparse.Namespace(**args)
 
