4f738068ce9b7201afa61e948a8d1f60c4476e3d
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2021-04-02 16:07:10 +0200

    modified flexible opencv requirments

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 requirements = [
-                'opencv-python==4.1.1.26',
-                'tqdm==4.36.1',
+                'opencv-python>=4.1.1.26',
+                'tqdm>=4.36.1',
                 'n2v==0.1.8 ',
                 'Keras==2.2.5',
-                'scikit-image==0.16.1',
-                'tiffile==2018.10.18'
+                'scikit-image>=0.16.1',
+                'tiffile>=2018.10.18'
                 ]
 setup(name='vtools',
       version='0.1',
